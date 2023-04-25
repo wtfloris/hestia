c60277a9a6a7eb835b17c63bb0a588e904f9df27
Floris Heringa <floris.heringa@prdct.nl>
2023-04-25 13:08:14 +0200

    Cleanup and add help functionality

--- hestia.py
+++ hestia.py
@@ -33,14 +33,22 @@ async def load_subs(update, context):
 async def transmit_error(e, update, context):
     log_msg = f'''Error loading file for subscriber: {update.effective_chat.username} ({update.effective_chat.id})
 {repr(e)}'''
-    logging.error(log_msg)
-    await context.bot.send_message(chat_id=OWN_CHAT_ID, text=log_msg)
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="Something went wrong there, but I've let @WTFloris know! He'll let you know once he's fixed his buggy-ass code."
-    )
-    return 1
 
+    logging.error(log_msg)
+    await context.bot.send_message(OWN_CHAT_ID, log_msg)
+    
+    message = "Something went wrong there, but I've let @WTFloris know! He'll let you know once he's fixed his buggy-ass code."
+    await context.bot.send_message(update.effective_chat.id, message)
+    
+async def privileged(update, context, command):
+    logging.info(f"Command {command} by ID {update.effective_chat.id}: {update.message.text}")
+
+    if not update.effective_chat.id in PRIVILEGED_USERS:
+        logging.warning(f"Unauthorized {command} attempted by ID {update.effective_chat.id}.")
+        await context.bot.send_message(update.effective_chat.id, "You're not allowed to do that!")
+        return 0
+    return 1
+    
 async def new_sub(subs, update, context):
     log_msg = f"New subscriber: {update.effective_chat.username} ({update.effective_chat.id})"
     logging.info(log_msg)
@@ -48,9 +56,8 @@ async def new_sub(subs, update, context):
     subs.add(update.effective_chat.id)
     with open(WORKDIR + "subscribers", 'wb') as file:
         pickle.dump(subs, file)
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="""Hi there!
+        
+    message ="""Hi there!
 
 I scrape real estate websites for new rental homes in and around Amsterdam. For more info on which websites I scrape with which parameters, say /websites.
 
@@ -59,7 +66,7 @@ Please note that some real estate websites provide paid members with early acces
 You are now recieving updates when I find something new! If you want me to stop, just say /stop.
 
 If you have any issues or questions, let @WTFloris know!"""
-    )
+    await context.bot.send_message(update.effective_chat.id, message)
 
 async def start(update, context):
     subs = await load_subs(update, context)
@@ -68,10 +75,8 @@ async def start(update, context):
         return 1
 
     if update.effective_chat.id in subs:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="You are already a subscriber, I'll let you know if I see any new rental homes online!"
-        )
+        message = "You are already a subscriber, I'll let you know if I see any new rental homes online!"
+        await context.bot.send_message(update.effective_chat.id, message)
     else:
         await new_sub(subs, update, context)
         
@@ -108,7 +113,7 @@ async def announce(update, context):
     for sub in subs:
         try:
             # If a user blocks the bot, this would throw an error and kill the entire broadcast
-            await context.bot.send_message(text=update.message.text[10:], chat_id=sub)
+            await context.bot.send_message(sub, update.message.text[10:])
         except BaseException as e:
             logging.warning(f"Exception while broadcasting announcement to {sub}: {repr(e)}")
             continue
@@ -122,11 +127,11 @@ async def websites(update, context):
         message += f"Search parameters: {target['info']['parameters']}\n"
         message += f"\n"
         
-    await context.bot.send_message(text=message[:-1], chat_id=update.effective_chat.id)
+    await context.bot.send_message(update.effective_chat.id, message[:-1])
     sleep(2)
     
     message = "If you want more information, you can also read my source code: https://github.com/wtfloris/hestia"
-    await context.bot.send_message(text=message, chat_id=update.effective_chat.id)
+    await context.bot.send_message(update.effective_chat.id, message)
     
 async def get_sub_info(update, context):
     if not privileged(update, context, "get_sub_info"): return
@@ -138,18 +143,17 @@ async def get_sub_info(update, context):
     message += f"Name: {chat.first_name} {chat.last_name}\n"
     message += f"Bio: {chat.bio}"
     
-    await context.bot.send_message(text=message, chat_id=update.effective_chat.id)
-    
-async def privileged(update, context, command):
-    logging.info(f"Command {command} by ID {update.effective_chat.id}: {update.message.text}")
+    await context.bot.send_message(update.effective_chat.id, message)
 
-    if not update.effective_chat.id in PRIVILEGED_USERS:
-        logging.warning(f"Unauthorized {command} attempted by ID {update.effective_chat.id}.")
-        await context.bot.send_message(text="You're not allowed to do that!", chat_id=update.effective_chat.id)
-        return 0
-    return 1
-        
+async def help(update, context):
+    message = f"I can do the following for you:\n"
+    message += "\n"
+    message += "\help - Show this message"
+    message += "\start - Subscribe to updates"
+    message += "\stop - Stop recieving updates"
+    message += "\websites - Show info about which websites I scrape"
     
+    await context.bot.send_message(update.effective_chat.id, message)
 
 if __name__ == '__main__':
     initialize()
@@ -161,6 +165,7 @@ if __name__ == '__main__':
     application.add_handler(CommandHandler("announce", announce))
     application.add_handler(CommandHandler("websites", websites))
     application.add_handler(CommandHandler("getsubinfo", get_sub_info))
+    application.add_handler(CommandHandler("help", help))
     application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), reply))
     
     application.run_polling()
