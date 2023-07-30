07f75be459cd65e50c4c3383acacea40b08edd31
Floris Heringa <floris.heringa@prdct.nl>
2023-07-30 14:03:58 +0200

    Add docker-compose-dev

--- build.sh
+++ build.sh
@@ -3,6 +3,17 @@ set -e
 docker build --tag wtfloris/hestia-bot:latest -f Dockerfile.bot .
 docker build --tag wtfloris/hestia-scraper:latest -f Dockerfile.scraper .
 
+ARGS=''
+
+if [[ $1 -eq -y ]]; then
+        if [[ $2 -eq dev ]]; then
+                ARGS='-f docker-compose-dev.yml'
+        fi
+        docker compose up -d $ARGS
+        exit
+fi
+
+
 read -p "Run the containers? [y/N]" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
