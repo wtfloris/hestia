958eb353a8ff2068292d78afda4eda65c0b02203
Floris Heringa <floris.heringa@prdct.nl>
2023-07-30 16:59:34 +0200

    Add /filter command

--- bot.py
+++ bot.py
@@ -230,18 +230,92 @@ async def status(update, context):
         message += f"{agency} ({target_id}): {count['count']} listings\n"
 
     await context.bot.send_message(update.effective_chat.id, message)
+    
+# TODO support for multiple cities
+async def filter(update, context):
+    cmd = update.message.text.split(' ')
+    
+    # '/filter' only or a mistake with no arguments
+    if len(cmd) == 1 or len(cmd) == 2:
+        sub = hestia.query_db(f"SELECT * FROM hestia.subscribers WHERE telegram_id == '{update.effective_chat.id}'", fetchOne=True)
+        filter_cities = hestia.query_db(f"SELECT filter_cities FROM hestia.meta", fetchOne=True)["filter_cities"]
+        
+        message = "**Currently, your filters are:**\n"
+        message += f"Min. price: {sub['filter_min_price']}\n"
+        message += f"Max. price: {sub['filter_max_price']}\n"
+        message += f"City: {sub['filter_cities'][0]}\n\n"
+        message += "**To change your filters, you can say:**\n"
+        message += "`/filter minprice 1200`\n"
+        message += "`/filter maxprice 1800`\n"
+        message += "`/filter city Amsterdam`\n\n"
+        message += "Options for the city filter are: "
+        # TODO use Telegram's UI to present a list of city options
+        
+        for city in filter_cities:
+            message += f"{city}, "
+            
+        # Skim the trailing comma
+        message = message[:-2]
+        
+    # Set filter
+    elif len(cmd) == 3:
+        if cmd[1] == "minprice":
+            try:
+                minprice = int(cmd[2])
+            except ValueError:
+                message = f"Invalid value: {cmd[2]} is not a number."
+                await context.bot.send_message(update.effective_chat.id, message)
+                return
+                
+            hestia.query_db(f"UPDATE subscribers SET filter_min_price = {minprice} WHERE telegram_id = '{update.effective_chat.id}'")
+            
+            message = f"Min. price filter set to {minprice}!"
+                
+        elif cmd[1] == "maxprice":
+            try:
+                maxprice = int(cmd[2])
+            except ValueError:
+                message = f"Invalid value: {cmd[2]} is not a number."
+                await context.bot.send_message(update.effective_chat.id, message)
+                return
+                
+            hestia.query_db(f"UPDATE subscribers SET filter_max_price = {maxprice} WHERE telegram_id = '{update.effective_chat.id}'")
+            
+            message = f"Max. price filter set to {maxprice}!"
+            
+        elif cmd[1] == "city":
+            city = cmd[2].capitalize()
+            filter_cities = hestia.query_db(f"SELECT filter_cities FROM hestia.meta", fetchOne=True)["filter_cities"]
+            
+            if city not in filter_cities:
+                message = f"Invalid city: {city}. Possibilities are: "
+                for city in filter_cities:
+                    message += f"{city}, "
+                # Skim the trailing comma
+                message = message[:-2]
+                return
+            
+            hestia.query_db(f"UPDATE subscribers SET filter_cities = [\"{city}\"] WHERE telegram_id = '{update.effective_chat.id}'")
+            
+            message = f"City filter set to {city}!"
+            
+        else:
+            message = "Invalid filter command"
+        
+    await context.bot.send_message(update.effective_chat.id, message)
 
 async def help(update, context):
-    message = f"I can do the following for you:\n"
+    message = f"**I can do the following for you:**\n"
     message += "\n"
     message += "/help - Show this message\n"
     message += "/start - Subscribe to updates\n"
-    message += "/stop - Stop recieving updates\n"
+    message += "/stop - Stop recieving updates\n\n"
+    message += "/filter - Show and modify your personal filters\n"
     message += "/websites - Show info about the websites I scrape"
     
     if privileged(update, context, "help", check_only=True):
         message += "\n\n"
-        message += "Admin commands:\n"
+        message += "**Admin commands:**\n"
         message += "/announce - Broadcast a message to all subscribers\n"
         message += "/getallsubs - Get all subscriber info\n"
         message += "/getsubinfo <sub_id> - Get info by subscriber ID\n"
@@ -262,6 +336,7 @@ if __name__ == '__main__':
     application.add_handler(CommandHandler("stop", stop))
     application.add_handler(CommandHandler("announce", announce))
     application.add_handler(CommandHandler("websites", websites))
+    application.add_handler(CommandHandler("filter", filter))
     application.add_handler(CommandHandler("getsubinfo", get_sub_info))
     application.add_handler(CommandHandler("getallsubs", get_all_subs))
     application.add_handler(CommandHandler("status", status))
