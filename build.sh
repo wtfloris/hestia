1b7e9ac85965b3c06dda26a1ca981d09c58df396
Floris Heringa <floris.heringa@prdct.nl>
2023-07-30 14:41:03 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 set -e
 
-docker build --tag wtfloris/hestia-bot:latest -f Dockerfile.bot .
-docker build --tag wtfloris/hestia-scraper:latest -f Dockerfile.scraper .
+TAG=latest
 
-ARGS=''
+if [[ $1 -eq dev ]]; then
+        TAG=dev
+        DEVARGS='-f docker-compose-dev.yml'
+fi
 
-if [[ $1 -eq -y ]]; then
-        if [[ $2 -eq dev ]]; then
-                DEVARGS='-f docker-compose-dev.yml'
-        fi
+docker build --tag wtfloris/hestia-bot:$TAG -f Dockerfile.bot .
+docker build --tag wtfloris/hestia-scraper:$TAG -f Dockerfile.scraper .
+
+if [[ $2 -eq -y ]]; then
         docker compose $DEVARGS up -d
-        exit
 fi
 
 read -p "Run the containers? [y/N]" -n 1 -r
