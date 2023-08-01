2a65fb06a15ab854e1ee865044b1937bc29658f4
Floris Heringa <floris.heringa@prdct.nl>
2023-08-01 12:41:55 +0200

    Big oof why do I publish all my mistakes

--- scraper.py
+++ scraper.py
@@ -166,7 +166,7 @@ async def scrape_site(target):
 
         for res in results:
             # Filter rented properties
-            if str(res.find(class_="page-price").contents[0]) = "verhuurd":
+            if str(res.find(class_="page-price").contents[0]) == "verhuurd":
                 continue
         
             home = {}
