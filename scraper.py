f6f417582a99d0688953655316bb6ac86e7f06c1
Floris Heringa <floris.heringa@prdct.nl>
2023-12-10 21:03:38 +0100

    Fix daily alerts when scraper is halted

--- scraper.py
+++ scraper.py
@@ -6,20 +6,15 @@ from datetime import datetime, timedelta
 from asyncio import run
 
 async def main():
-    if hestia.check_scraper_halted():
-        logging.warning("Scraper is halted.")
-        exit()
-        
-    targets = hestia.query_db("SELECT * FROM hestia.targets WHERE enabled = true")
     
-    # Once a day at 8pm, check some stuff and send an alert
-    if datetime.now().hour == 20 and datetime.now().minute < 4:
+    # Once a day at 7pm, check some stuff and send an alert if necessary
+    if datetime.now().hour == 19 and datetime.now().minute < 4:
         message = ""
     
         if hestia.check_dev_mode():
             message += "\n\nDev mode is enabled."
             
-        if hestia.check_scraper_halted():
+        if hestia.check_scraper_halted() and 'dev' not in hestia.APP_VERSION:
             message += "\n\nScraper is halted."
     
         # Check if the donation link is expiring soon
@@ -30,6 +25,12 @@ async def main():
             
         if message:
             await hestia.BOT.send_message(text=message[2:], chat_id=secrets.OWN_CHAT_ID)
+    
+    if hestia.check_scraper_halted():
+        logging.warning("Scraper is halted.")
+        exit()
+
+    targets = hestia.query_db("SELECT * FROM hestia.targets WHERE enabled = true")
 
     for target in targets:
         try:
