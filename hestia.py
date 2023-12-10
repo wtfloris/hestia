997ef5a418d4f9d9dd47732b56f80d79641cf2a6
Floris Heringa <floris.heringa@prdct.nl>
2023-12-10 22:10:42 +0100

    Funda: handle house number suffix better

--- hestia.py
+++ hestia.py
@@ -301,7 +301,10 @@ class HomeResults:
             
             home.address = f"{res['_source']['address']['street_name']} {res['_source']['address']['house_number']}"
             if "house_number_suffix" in res["_source"]["address"].keys():
-                home.address += f"{res['_source']['address']['house_number_suffix']}"
+                suffix = res["_source"]["address"]["house_number_suffix"]
+                if '-' not in suffix and '+' not in suffix:
+                    suffix = f" {suffix}"
+                home.address += f"{suffix}"
             
             home.city = res["_source"]["address"]["city"]
             home.url = "https://funda.nl" + res["_source"]["object_detail_page_relative_url"]
